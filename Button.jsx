8ab67f007e4f40917b863f31d58eee55caa18ed2
Arthur Bressan <125163192+Arthur-Bressan@users.noreply.github.com>
2025-06-16 19:53:30 -0300

    Bug Fix, Suggestions container
    Resposta Vazia e Container de sugestões conforme digitar o agente e ordem alfabética aplicada.

---
+++ Button.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+
+function Button({ onClick, text }) {
+
+    return (
+        <>
+            <button onClick={onClick}>{text}</button>
+        </>
+    )
+}
+
+export default Button
